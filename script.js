16e87ceb27dae4d89a0f8809d2830f7b0b656ad3
Tussis88 <andreab88@hotmail.it>
2024-04-26 18:12:49 +0200

    aggiunta funzione codeParse

--- script.js
+++ script.js
@@ -10,10 +10,15 @@ inputDom.focus();
 inputDom.addEventListener("keydown", (e) => {
   if (e.key === "Enter" || e.key === "Tab") {
     let inputValue = e.target.value;
-    console.log(inputValue);
+    result = codeParser(inputValue);
+    console.log(`valore: ${inputValue} || esito: ${result}`);
     e.target.value = "";
     inputDom.focus();
   }
 });
 
-
+function codeParser(stringCode) {
+  if (stringCode.length > 3 && stringCode[2] === "." && stringCode.length < 8) return "codice Brevi";
+  if (stringCode.length > 6) return "seriale";
+  return "invalido";
+}
