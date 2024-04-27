6ac82151951d9186cb2dd1672b7cee40772b6ff0
Tussis88 <andreab88@hotmail.it>
2024-04-27 15:25:56 +0200

    creata funzione tableGenerator
    ora il programma crea automanticamente la tabella relativa al codice
    Brevi e aggiunge i seriali all'ultima tabella inserita

--- script.js
+++ script.js
@@ -1,24 +1,85 @@
+// Disattiva la funzione di default del tasto tab
 document.addEventListener("keydown", (e) => {
   if (e.key === "Tab") {
     e.preventDefault();
     e.stopPropagation();
   }
 });
-const inputDom = document.querySelector("#inputString");
 
+// GLOBAL VARIABLES
+let currentCode;
+
+// DOM
+// Input
+const inputDom = document.querySelector("#inputString");
 inputDom.focus();
 inputDom.addEventListener("keydown", (e) => {
   if (e.key === "Enter" || e.key === "Tab") {
-    let inputValue = e.target.value;
-    result = codeParser(inputValue);
-    console.log(`valore: ${inputValue} || esito: ${result}`);
+    let result = codeParser(e.target.value);
+    tableGenerator(result);
+    console.log(`valore: ${result[0]} || esito: ${result[1]}`);
     e.target.value = "";
     inputDom.focus();
   }
 });
 
-function codeParser(stringCode) {
-  if (stringCode.length > 3 && stringCode[2] === "." && stringCode.length < 8) return "codice Brevi";
-  if (stringCode.length > 6) return "seriale";
-  return "invalido";
+// FUNZIONI
+// distingue tra codice Brevi e seriali
+function codeParser(input) {
+  if (input.length > 3 && input[2] === "." && input.length < 8)
+    return [input, "brevi"];
+  if (input.length > 6) return [input, "seriale"];
+  return [input, "invalid"];
+}
+
+// aggiunge i dati alla tabella corretta
+function tableGenerator(data) {
+  const tables = document.querySelectorAll("table");
+  const tableContainer = document.querySelector("#tableContainer");
+
+  if (data[1] === "brevi") {
+    // gli id non possono contenere punti  e css non supporta id che iniziano con numeri
+    currentCode = "c" + data[0].replace(".", "dot");
+    tablesArray = [...tables];
+    tableId = tablesArray.filter((table) => table.id === currentCode);
+    if (tableId.length > 0) {
+      console.log(tableId);
+    } else {
+      const newTable = document.createElement("table");
+      newTable.id = currentCode;
+      tableContainer.appendChild(newTable);
+      const newHead = document.createElement("thead");
+      newTable.appendChild(newHead);
+      const newBody = document.createElement("tbody");
+      newTable.appendChild(newBody);
+      const newRow = document.createElement("tr");
+      newHead.appendChild(newRow);
+
+      const firstHead = document.createElement("th");
+      firstHead.scope = "col";
+      firstHead.textContent = "-";
+      newRow.appendChild(firstHead);
+
+      const secondHead = document.createElement("th");
+      secondHead.scope = "col";
+      secondHead.textContent = data[0];
+      newRow.appendChild(secondHead);
+    }
+  } else if (data[1] === "seriale") {
+    const currentTable = document.querySelector(`#${currentCode}`);
+    console.log(currentTable);
+    if (currentTable) {
+      const newRow = document.createElement("tr");
+      currentTable.querySelector("tbody").appendChild(newRow);
+      
+      const firstCell = document.createElement("td");
+      firstCell.textContent = "-";
+      newRow.appendChild(firstCell);
+
+      const secondCell = document.createElement("td");
+      secondCell.textContent = data[0];
+      newRow.appendChild(secondCell);
+    }
+  }
+  
 }
